c51dee68c32d672ee7935f53f9b3c783af2c04ab
pepecuencar <pepecuencar@gmail.com>
2020-09-14 14:08:59 -0500

    Pushing Cognito config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: ' us-east-1_wS4WW87Uv', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6vim0cvotle5fnrc87hrr5avc3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
+
+
